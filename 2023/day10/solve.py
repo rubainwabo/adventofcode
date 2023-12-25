afaadf35ca274d52ca591969d2d06aab37a506c0
rubainwabo <urbainwabo@gmail.com>
2023-12-25 02:59:44 +0100

    day10

--- 2023/day10/solve.py
+++ 2023/day10/solve.py
@@ -20,7 +20,8 @@ D = {
     '-': [0,0,-1,1],
     '|': [1,-1,0,0],
     'J': [1,0,-1,0],
-    '.': [0,0,0,0]
+    '.': [0,0,0,0],
+    'S': [1,-1,-1,1],
 }
 
 lenI = len(M)
@@ -29,53 +30,61 @@ def find_next():
     i=s[0]
     j=s[1]
     #North
-    if i-1>=0 and 1-D[M[i-1][j]][0]==0:
-        return tuple((i-1, j))
+    if i-1>=0 and 1+D[M[i-1][j]][1]==0:
+        return tuple((i-1, j, 2))
     #South
-    elif i+1<lenI and 1+D[M[i+1][j]][1]==0:
-        return tuple((i+1, j))
+    elif i+1<lenI and 1-D[M[i+1][j]][0]==0:
+        return tuple((i+1, j, 1))
     #West
-    elif j-1>=0 and 1+D[M[i][j-1]][2]==0:
-        return tuple((i, 1))
+    elif j-1>=0 and 1-D[M[i][j-1]][3]==0:
+        return tuple((i, j-1, 4))
     #East
-    elif j+1<lenJ and 1-D[M[i][j+1]][3]==0:
-        return tuple((i, j+1))
+    elif j+1<lenJ and 1+D[M[i][j+1]][2]==0:
+        return tuple((i, j+1, 3))
     return None
 s = find_next()
 i=s[0]
 j=s[1]
-s=M[i][j]
+pt=M[i][j]
 n=0
+#N=1 S=2 W=3 E=4
+visited=s[2]
 while True:
-    if s=='S':
+    if pt=='S':
         break
-    elif s=='L':
-        if i-1>=0 and 1-D[M[i-1][j]][0]==0:
-            s=M[i-1][j]
-        elif j+1<lenJ and 1-D[M[i][j+1]][3]==0:
-            s=M[i][j+1]
-    elif s=='-':
-        if j-1>=0 and 1+D[M[i][j-1]][2]==0:
-            s=M[i][j-1]
-        elif j+1<lenJ and 1-D[M[i][j+1]][3]==0:
-            s=M[i][j+1]
-    elif s=='F':
-        if i+1<lenI and 1+D[M[i+1][j]][3]==0:
-            s=M[i+1][j]
-        elif j+1<lenJ and 1-D[M[i][j+1]][1]==0:
-            s=M[i][j+1]
-    elif s=='J':
-        if i-1>=0 and 1-D[M[i-1][j]][0]==0:
-            s=M[i-1][j]
-        elif j-1>=0 and 1+D[M[i][j-1]][2]==0:
-            s=M[i][j-1]
-    elif s=='|':
-        if i-1>=0 and 1-D[M[i-1][j]][0]==0:
-            s=M[i-1][j]
-        elif i+1<lenI and 1+D[M[i+1][j]][1]==0:
-            s=M[i+1][j]
-    elif s=='7':
-        if i+1<lenI and 1+D[M[i+1][j]][1]==0:
-            s=M[i+1][j]
-        elif j-1>=0 and 1+D[M[i][j-1]][2]==0:
-            s=M[i][j-1]
+    elif pt=='L':
+        if i-1>=0 and 1+D[M[i-1][j]][1]==0 and visited!=1:
+            s=tuple((i-1, j, 2))
+        elif j+1<lenJ and 1+D[M[i][j+1]][2]==0 and visited!=4:
+            s=tuple((i, j+1, 3))
+    elif pt=='-':
+        if j-1>=0 and 1-D[M[i][j-1]][3]==0 and visited!=3:
+            s=tuple((i, j-1, 4))
+        elif j+1<lenJ and 1+D[M[i][j+1]][2]==0 and visited!=4:
+            s=tuple((i, j+1, 3))
+    elif pt=='F':
+        if i+1<lenI and 1-D[M[i+1][j]][0]==0 and visited!=2:
+            s=tuple((i+1, j, 1))
+        elif j+1<lenJ and 1+D[M[i][j+1]][2]==0 and visited!=4:
+            s=tuple((i, j+1, 3))
+    elif pt=='J':
+        if i-1>=0 and 1+D[M[i-1][j]][1]==0 and visited!=1:
+            s=tuple((i-1, j, 2))
+        elif j-1>=0 and 1-D[M[i][j-1]][3]==0 and visited!=3:
+            s=tuple((i, j-1, 4))
+    elif pt=='|':
+        if i-1>=0 and 1+D[M[i-1][j]][1]==0 and visited!=1:
+            s=tuple((i-1, j, 2))
+        elif i+1<lenI and 1-D[M[i+1][j]][0]==0 and visited!=2:
+            s=tuple((i+1, j, 1))
+    elif pt=='7':
+        if i+1<lenI and 1-D[M[i+1][j]][0]==0 and visited!=2:
+            s=tuple((i+1, j, 1))
+        elif j-1>=0 and 1-D[M[i][j-1]][3]==0 and visited!=3:
+            s=tuple((i, j-1, 4))
+    i=s[0]
+    j=s[1]
+    visited=s[2]
+    pt=M[i][j]
+    n+=1
+print(int(n/2)+1)
